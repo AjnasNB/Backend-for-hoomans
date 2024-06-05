116728544f03dde1c47187d520950790b43cef9e
AjnasNB <ajnasnb@gmail.com>
2024-06-05 13:18:01 +0530

    update

--- api/server.js
+++ api/server.js
@@ -4,13 +4,9 @@ const cors = require('cors');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
