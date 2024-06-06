891e187e6d0aa72e91fce8bfc28849b4bf9816d3
AjnasNB <ajnasnb@gmail.com>
2024-06-06 07:57:58 +0530

    update

--- api/server.js
+++ api/server.js
@@ -5,7 +5,19 @@ const apiRoutes = require('./routes/api');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+
+// Configure CORS to allow requests from any origin
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Logging middleware to help debug
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
 app.use('/api', apiRoutes);
 
